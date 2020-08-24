1c1b8fd43f4ef9703b13b022e468422015894cab
duy.phan <duy.phan@drjoy.vn>
2020-08-24 15:29:01 +0700

    Update design

---
+++ DropshipEsty/src/main/java/com/ping/TestDesignPanel.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ping;
+
+import com.utils.OSUtil;
+import com.ping.view.BasePanel;
+import com.ping.view.CustomImageDesignPannel;
+import com.ping.view.DesignPanel;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author duyuno
+ */
+public class TestDesignPanel {
+
+    JFrame jFrame;
+
+    JPanel container;
+
+    public void init() {
+        initPannel();
+    }
+    
+    public void hideCurrentMode() {
+        container.removeAll();
+        jFrame.dispatchEvent(new WindowEvent(jFrame, WindowEvent.WINDOW_CLOSING));
+    }
+    
+    public void initPannel() {
+        jFrame = new JFrame();
+//        container = new JPanel();
+        OSUtil.setAppTitle(jFrame);
+        DesignPanel designPanel = new DesignPanel();
+        jFrame.add(designPanel);
+//        CustomImageDesignPannel customImageDesignPannel = new CustomImageDesignPannel();
+//        jFrame.add(customImageDesignPannel);
+//        
+//        try {
+//            BufferedImage img1 = ImageIO.read(new File("3.jpg"));
+//            BufferedImage img2 = ImageIO.read(new File("1.png"));
+//            
+//            customImageDesignPannel.setContainWidth(600);
+//            customImageDesignPannel.setContainHeight(600);
+//            customImageDesignPannel.setPreferredSize(new Dimension(600, 600));
+//            
+//            customImageDesignPannel.setImgBackground(img1);
+//            customImageDesignPannel.setImgItem(img2);
+//            customImageDesignPannel.setItemWidth(100);
+//            customImageDesignPannel.setItemHeight(100);
+//            customImageDesignPannel.repaint();
+//        } catch (IOException ex) {
+//            Logger.getLogger(DesignPanel.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        
+
+        jFrame.pack();
+        jFrame.setResizable(false);
+        jFrame.setVisible(true);
+        
+        jFrame.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent evt) {
+                if (OSUtil.isWindows()) {
+                    String sc = "taskkill /F /IM chromedriver.exe";
+
+                    try {
+                        Process p = Runtime.getRuntime().exec(sc);
+                    } catch (IOException ex) {
+                        Logger.getLogger(TestDesignPanel.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        });
+    }
+
+    public static void main(String args[]) {
+        TestDesignPanel startClientApp = new TestDesignPanel();
+        startClientApp.init();
+    }
+
+    public static void addBasePanel(JFrame jFrame, JPanel container, BasePanel basePanel) {
+        container.add(basePanel);
+        jFrame.setTitle(basePanel.getTitle());
+    }
+
+    public static JMenuItem createMenuItem(BasePanel basePanel) {
+        JMenuItem jItem = new JMenuItem(basePanel.getTitle());
+        jItem.setActionCommand(basePanel.getMenuActionCommand());
+
+        return jItem;
+    }
+}
---
+++ DropshipEsty/src/main/java/com/ping/view/CustomImageDesignPannel.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ping.view;
+
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author duy.phan
+ */
+public class CustomImageDesignPannel extends JPanel {
+
+    private int containSize;
+    private int bgSize;
+    private int itemSize;
+    private int itemSizeDisplay;
+    BufferedImage imgBackground;
+    BufferedImage imgItem;
+    private Point itemPoint = null;
+    private float ratio;
+    
+    public void setImgBackground(BufferedImage imgBackground) {
+        this.imgBackground = imgBackground;
+        bgSize = imgBackground.getWidth();
+        ratio = containSize * 1f / bgSize;
+    }
+
+    public void setContainSize(int containSize) {
+        this.containSize = containSize;
+    }
+
+    public void setItemSize(int itemSize) {
+        this.itemSize = itemSize;
+        
+        if (itemPoint == null) {
+            itemPoint = new Point(itemSize / 2, itemSize / 2);
+        }
+    }
+    
+    public void setItemSizeDisplay(int itemSizeDisplay) {
+        this.itemSizeDisplay = itemSizeDisplay;
+        this.itemSize = (int)(itemSizeDisplay / ratio);
+    }
+
+    public void setPoint(Point point) {
+        this.itemPoint = point;
+    }
+    
+    public void setImgItem(BufferedImage imgItem) {
+        this.imgItem = imgItem;
+    }
+    
+    public int getItemDisplaySize() {
+        return (int) (itemSize * ratio);
+    }
+
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
+        if (imgBackground != null) {
+            
+            g.drawImage(imgBackground, 0, 0, containSize, containSize, this);
+
+            if (imgItem != null && itemPoint != null) {
+                g.drawImage(imgItem, itemPoint.x - itemSizeDisplay / 2, itemPoint.y - itemSizeDisplay / 2, itemSizeDisplay, itemSizeDisplay, this);
+            }
+        }
+    }
+
+}
